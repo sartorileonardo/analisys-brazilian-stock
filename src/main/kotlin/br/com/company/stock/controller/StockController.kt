ae8855b5495e86e86b5d0a4037eff215e5dfcfec
Leonardo Sartori <leonardogt4@hotmail.com>
2022-03-13 20:43:26 -0300

    Adicionado novos testes para codigos 200, 400, 404 e 500

--- src/main/kotlin/br/com/company/stock/controller/StockController.kt
+++ src/main/kotlin/br/com/company/stock/controller/StockController.kt
@@ -2,9 +2,6 @@ package br.com.company.stock.controller
 
 import br.com.company.stock.dto.StockAnalysisDto
 import br.com.company.stock.service.AcaoService
-import io.swagger.annotations.ApiOperation
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
@@ -13,15 +10,6 @@ import reactor.core.publisher.Mono
 @RestController
 @RequestMapping("/stock")
 class StockController @Autowired constructor(val service: AcaoService){
-    @ApiOperation(value = "Display with stock analysis", response = StockAnalysisDto::class)
-    @ApiResponses(
-        value = [
-            ApiResponse(responseCode = "200", description = "Sucessful operation"),
-            ApiResponse(responseCode = "400", description = "Invalid input with ticker field"),
-            ApiResponse(responseCode = "404", description = "Ticker not found"),
-            ApiResponse(responseCode = "500", description = "Internal server error"),
-        ]
-    )
     @GetMapping("/data/{ticker}")
     fun getAnalise(@PathVariable ticker: String): Mono<StockAnalysisDto> {
         return service.getAnalise(ticker)
--- src/test/kotlin/br/com/company/stock/controller/StockControllerTest.kt
+++ src/test/kotlin/br/com/company/stock/controller/StockControllerTest.kt
@@ -1,5 +1,6 @@
 package br.com.company.stock.controller
 
+import br.com.company.stock.config.StockParametersApiConfig
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -7,26 +8,46 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class StockControllerTest(@Autowired val mockMvc: MockMvc) {
-
+class StockControllerTest(
+    @Autowired val mockMvc: MockMvc,
+    @Autowired val config: StockParametersApiConfig
+    ) {
+    private val urlApi: String = "/stock/data/"
+    private val exampleValidTicker: String = "petro4"
+    private val exampleInvalidTicker: String = "petroo"
+    private val exampleNotFoundTicker: String = "petro8"
 
     @Test
-    fun findAllWithSucess() {
-        mockMvc.perform(get("/stock")
+    fun findAnalisysWhenReturnIs200Code() {
+        mockMvc.perform(get("${urlApi}${exampleValidTicker}")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk)
     }
 
     @Test
-    fun findData() {
-        mockMvc.perform(get("/stock/data")
+    fun findAnalisysWhenReturnIs400Code(){
+        mockMvc.perform(get("${urlApi}${exampleInvalidTicker}")
             .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk)
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun findAnalisysWhenReturnIs404Code(){
+        mockMvc.perform(get("${urlApi}${exampleNotFoundTicker}")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun findAnalisysWhenReturnIs500Code(){
+        config.url = "<URL_API_RESET>"
+        mockMvc.perform(get("${urlApi}${exampleValidTicker}")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError)
     }
 
 }
