cf148f998cb4aae4e002513a6482a2717825fe63
Leonardo Sartori <leonardogt4@hotmail.com>
2022-04-10 09:20:36 -0300

    Ajuste de validacao de ticker

--- src/main/kotlin/br/com/company/stock/validation/TickerValidation.kt
+++ src/main/kotlin/br/com/company/stock/validation/TickerValidation.kt
@@ -6,32 +6,33 @@ import org.springframework.util.ObjectUtils
 class TickerValidation {
     fun validarTicker(ticker: String) {
         validarSeForVazio(ticker)
-        validarSePossuiSomenteNumeros(ticker)
-        validarSePossuiSomenteLetras(ticker)
-        validarSeTerminaComDigitoAceito(ticker)
-        validarSeForBDR(ticker)
+        val tickerTrim = ticker.trim()
+        validarSePossuiSomenteNumeros(tickerTrim)
+        validarSePossuiSomenteLetras(tickerTrim)
+        validarSeTerminaComDigitoAceito(tickerTrim)
+        validarSeForBDR(tickerTrim)
     }
 
     private fun validarSeForBDR(ticker: String) {
-        if (listOf("32", "33", "34", "35").contains(ticker.trim().substring(ticker.trim().length - 2))) {
+        if (listOf("32", "33", "34", "35").contains(ticker.substring(ticker.length - 2))) {
             throw BusinessException("O sistema não suporta ticker de BDR's, tente novamente com um ticker de empresa brasileira!")
         }
     }
 
     private fun validarSeTerminaComDigitoAceito(ticker: String) {
-        if (!listOf("3", "4", "11").contains(ticker.trim().last().toString())) {
+        if (!listOf("3", "4").contains(ticker.last().toString()) && ticker.substring(ticker.length-2) != "11") {
             throw BusinessException("Ticker deve terminar com 3, 4 ou 11, exemplo: ABEV3, BBDC4, TAEE11!")
         }
     }
 
     private fun validarSePossuiSomenteLetras(ticker: String) {
-        if (ticker.trim().matches(Regex("^[a-zA-Z]+\$"))) {
+        if (ticker.matches(Regex("^[a-zA-Z]+\$"))) {
             throw BusinessException("Ticker não pode ser somente letras!")
         }
     }
 
     private fun validarSePossuiSomenteNumeros(ticker: String) {
-        if (ticker.trim().matches(Regex("\\d+"))) {
+        if (ticker.matches(Regex("\\d+"))) {
             throw BusinessException("Ticker não pode ser somente numeros!")
         }
     }
